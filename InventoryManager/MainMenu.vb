59ff72da2fa42403c1a8902f0b4fd4c11526a721
Shoumik Banerjee <sbanerjeev2@gmail.com>
2017-10-25 16:00:23 -0400

    Succesfullly tested connection to local database.

--- InventoryManager/MainMenu.vb
+++ InventoryManager/MainMenu.vb
@@ -1,3 +1,23 @@
-﻿Public Class MainMenu
+﻿Imports System.Data.SqlClient
 
+Public Class MainMenu
+    'Private connectionString As String = "Server=INVSUXS-D; Database=INVENTORYSQL; User Id=SQLadmin; Password=1NV3nt0ry5uXX5"
+    Private connectionString As String = "Server=localhost\INVENTORYSQL;Database=master;Trusted_Connection=True;"
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+
+    Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myConn = New SqlConnection(connectionString)
+        myConn.Open()
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT first_name FROM Employee"
+        myReader = myCmd.ExecuteReader()
+        Dim results As String
+        Do While myReader.Read()
+            results += myReader.GetString(0) + vbCrLf
+        Loop
+        MsgBox(results)
+    End Sub
 End Class
